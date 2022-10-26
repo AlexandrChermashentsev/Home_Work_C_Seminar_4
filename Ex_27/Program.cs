a7d2515f91e1a9108071279cb6e1f96f96b73b17
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-10-26 17:15:21 +0500

    Add Ex_27

---
+++ Ex_27/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+Console.WriteLine("Enter the number");
+int userNumber = int.Parse(Console.ReadLine());
+int length = userNumber.ToString().Length;
+int result = 0;
+int number = userNumber;
+for (int i = 0; i < length; i++)
+{
+    result = result + userNumber % 10;
+    userNumber = userNumber / 10;
+}
+Console.WriteLine($"сумма цифр в числе {number} ровна {result}");
